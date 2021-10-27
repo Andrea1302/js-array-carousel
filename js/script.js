7360db5d25fc9935c762fbb4e46d514942db9576
Andrea Scarpa <scarpandrea.1996@hotmail.com>
2021-10-27 17:28:53 +0200

    Creazione html da js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+const immaginePrincipale = document.querySelector(".items");
+console.log(immaginePrincipale);
+const immagineDaScegliere = document.getElementById("wich_item")
 
 const items = [
     'img/01.jpg',
@@ -13,7 +16,7 @@ const title = [
     'Gran Bretagna',
     'Germania',
     'Paradise'
-]
+];
 
 const text = [
     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
@@ -21,4 +24,19 @@ const text = [
     'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
     'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-]
+];
+
+// // Immagini di default 
+// immaginePrincipale.innerHTML += `<img src="img/01.jpg" alt=""> `;
+// immagineDaScegliere.innerHTML += `<img src="img/01.jpg" alt="">`;
+
+let item = "";
+let whichItem = "";
+
+for ( let i = 0 ; i < items.length ; i++) {
+    item += `<img src="${items[i]}" alt="">`;
+    whichItem += `<img src="${items[i]}" alt="">`
+}   
+
+immaginePrincipale.innerHTML = item;
+immagineDaScegliere.innerHTML = whichItem;
